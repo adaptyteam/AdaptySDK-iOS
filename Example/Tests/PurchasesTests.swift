c10c52c0ae87f46d9dea5ca50beed0808df4f1d8
Rustam Galiullin <ihityouback@gmail.com>
2021-12-09 18:26:04 +0300

    ADAPTY-2574 - added test for consumable purchase

--- Example/Tests/PurchasesTests.swift
+++ Example/Tests/PurchasesTests.swift
@@ -24,7 +24,7 @@ class PurchasesTests: XCTestCase {
     }
 
     
-    func testMakePurchase() throws {
+    func testMakeSubscriptionPurchase() throws {
         let session = try SKTestSession(configurationFileNamed: "StoreKitConfiguration")
         session.resetToDefaultState()
         session.disableDialogs = true
@@ -60,6 +60,45 @@ class PurchasesTests: XCTestCase {
         let transaction = session.allTransactions()[0]
         expect(transaction.productIdentifier).to(equal(product.vendorProductId))
         expect(transaction.autoRenewingEnabled).to(beTrue())
+        expect(transaction.state).to(equal(.purchased))
+    }
+    
+    func testMakeConsumablePurchase() throws {
+        let session = try SKTestSession(configurationFileNamed: "StoreKitConfiguration")
+        session.resetToDefaultState()
+        session.disableDialogs = true
+        session.clearTransactions()
+        
+        let productData = try DataProvider().jsonDataNamed("Product_coins")
+        let json = try JSONSerialization.jsonObject(with: productData, options: []) as! [String: Any]
+        let product = try ProductModel(json: json)!
+        
+        let paywallsStub = Stub(statusCode: 200, jsonFileName: "PaywallArray",
+                                error: nil, urlMatcher: "purchase-containers")
+        MockURLProtocol.addStab(paywallsStub)
+        
+        let validationStub = Stub(statusCode: 200, jsonFileName: "ValidationResponse",
+                                  error: nil, urlMatcher: "validate")
+        MockURLProtocol.addStab(validationStub)
+        
+        let requestManager = RequestManager(session: .mock)
+        let apiManager = ApiManager(requestManager:requestManager)
+        let iapManager = IAPManager(apiManager: apiManager)
+        iapManager.startObservingPurchases(nil)
+        
+        waitUntil(timeout: .seconds(10)) { done in
+            iapManager.makePurchase(product: product, offerId: nil, completion: {
+                purchaserInfo, receipt, appleValidationResult, product, error in
+                expect(purchaserInfo).notTo(beNil())
+                done()
+            })
+        }
+
+        expect(session.allTransactions().count).to(equal(1))
+
+        let transaction = session.allTransactions()[0]
+        expect(transaction.productIdentifier).to(equal(product.vendorProductId))
+        expect(transaction.autoRenewingEnabled).to(beFalse())
         expect(transaction.state).to(equal(.purchased))
     }
 }
