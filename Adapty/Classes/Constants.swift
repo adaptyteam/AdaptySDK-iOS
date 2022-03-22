c610910eac126698ab924243d46d9df2483ff876
Rustam Galiullin <ihityouback@gmail.com>
2022-03-22 20:20:52 +0300

    Bumped app version to 1.16.10

--- Adapty/Classes/Constants.swift
+++ Adapty/Classes/Constants.swift
@@ -58,7 +58,7 @@ enum Constants {
     }
     
     enum Versions {
-        static let SDKVersion = "1.16.9"
+        static let SDKVersion = "1.16.10"
         static let SDKBuild = 1
     }
     
