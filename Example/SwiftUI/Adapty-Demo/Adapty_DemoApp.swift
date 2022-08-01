015f25e0887c2baf3104ce9f69ccb54ab7c4c3e0
Elena Gordienko <gloomylego@gmail.com>
2022-08-01 21:54:30 +0400

    update app

--- Example/SwiftUI/Adapty-Demo/Adapty_DemoApp.swift
+++ Example/SwiftUI/Adapty-Demo/Adapty_DemoApp.swift
@@ -6,15 +6,48 @@
 //  Copyright © 2022 Adapty. All rights reserved.
 //
 
+import Adapty
 import SwiftUI
+import UIKit
 
 @main
 struct Adapty_DemoApp: App {
+    @Environment(\.scenePhase) var scenePhase
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @StateObject var paywallService = PaywallService()
+    @StateObject var userService = UserService()
+    @State private var showingPaywall = false
+    
+    init() {
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color.Palette.accent)]
+    }
+    
+    // MARK: - UI
     
     var body: some Scene {
         WindowGroup {
-            EmptyView()
+            ContentView(showingPaywall: $showingPaywall)
+                .environmentObject(paywallService)
+                .environmentObject(userService)
+                .onAppear {
+                    if Adapty.delegate == nil {
+                        Adapty.delegate = userService
+                    }
+                    paywallService.getPaywalls()
+                }
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
+    private func processScenePhaseChange(to phase: ScenePhase) {
+        switch phase {
+        case .active, .background:
+            break
+        case .inactive:
+            showingPaywall = false
+        @unknown default:
+            break
         }
     }
 }
