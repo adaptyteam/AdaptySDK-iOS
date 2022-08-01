c449be531d671749ea9a77bb80e0d2dffb855cef
Elena Gordienko <gloomylego@gmail.com>
2022-08-01 21:00:10 +0400

    initial commit

---
+++ Example/SwiftUI/Adapty-Demo/Adapty_DemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  Adapty_DemoApp.swift
+//  Adapty-Demo
+//
+//  Created by Elena Gordienko on 01.08.22.
+//  Copyright © 2022 Adapty. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Adapty_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/SwiftUI/Adapty-Demo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Adapty-Demo
+//
+//  Created by Elena Gordienko on 01.08.22.
+//  Copyright © 2022 Adapty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
