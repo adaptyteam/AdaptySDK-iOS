697af9d4aa50afbd025ec1f5eb06aaae8c81896b
Elena Gordienko <gloomylego@gmail.com>
2022-08-01 21:31:58 +0400

    add models

---
+++ Example/SwiftUI/Adapty-Demo/Model/PaywallViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PaywallViewModel.swift
+//  Adapty-Demo
+//
+//  Created by Elena Gordienko on 01.08.22.
+//  Copyright © 2022 Adapty. All rights reserved.
+//
+
+import Foundation
+
+struct PaywallViewModel: Equatable {
+    let iconName: String
+    let description: String
+    let buyActionTitle: String
+    let cancelActionTitle: String
+    let restoreActionTitle: String
+}
