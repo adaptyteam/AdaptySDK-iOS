4b74d4c2db5c572b93f2c38363f66e5a60ea0054
Elena Gordienko <gloomylego@gmail.com>
2022-08-01 21:29:07 +0400

    add utils for assets

---
+++ Example/SwiftUI/Adapty-Demo/Utils/Color+Palette.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Palette.swift
+//  Adapty-Demo
+//
+//  Created by Elena Gordienko on 01.08.22.
+//  Copyright © 2022 Adapty. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    enum Palette {
+        static let background = Color.black
+        static let accent = Color.accentColor
+        static let accentContent = Color.white
+    }
+}
