5f2f23d183d1aaafe740a9625baa5659ca966391
Mark Onyschuk <mark.onyschuk@gmail.com>
2022-09-03 08:42:33 -0400

    Add macOS 10.15 condition, storefrontCountryCode

--- Adapty/Classes/IAPManager.swift
+++ Adapty/Classes/IAPManager.swift
@@ -65,7 +65,7 @@ class IAPManager: NSObject {
     }
 
     var storefrontCountryCode: String? {
-        if #available(iOS 13.0, *) {
+        if #available(iOS 13.0, macOS 10.15, *) {
             return SKPaymentQueue.default().storefront?.countryCode
         } else {
             return nil
