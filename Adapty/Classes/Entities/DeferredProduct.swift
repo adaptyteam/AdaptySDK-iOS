388d2a1facc64eefdecd880efb360bd848652084
Alexey Goncharov <x40om@gmail.com>
2022-11-07 13:22:54 +0100

    mac os support fix

--- Adapty/Classes/Entities/DeferredProduct.swift
+++ Adapty/Classes/Entities/DeferredProduct.swift
@@ -32,7 +32,7 @@ extension DeferredProduct: CustomStringConvertible {
 extension DeferredProduct {
     init(skProduct: SKProduct, payment: SKPayment?) {
         let promotionalOfferId: String?
-        if #available(iOS 12.2, *), let discountId = payment?.paymentDiscount?.identifier {
+        if #available(iOS 12.2, macOS 10.14.4, *), let discountId = payment?.paymentDiscount?.identifier {
             promotionalOfferId = discountId
         } else {
             promotionalOfferId = nil
