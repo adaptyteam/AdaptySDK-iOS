cda1200ba13ebe6e41fc53c2270f9a8b62df768d
Alexey Goncharov <x401om@gmail.com>
2023-01-20 12:50:25 +0100

    Update getPaywall method Documentation

--- Sources/Adapty.swift
+++ Sources/Adapty.swift
@@ -148,6 +148,9 @@ extension Adapty {
     /// - Parameters:
     ///   - id: The identifier of the desired paywall. This is the value you specified when you created the paywall in the Adapty Dashboard.
     ///   - locale: The identifier of the paywall [localization](https://docs.adapty.io/docs/paywall#localizations).
+    ///             This parameter is expected to be a language code composed of one or more subtags separated by the "-" character. The first subtag is for the language, the second one is for the region (The support for regions will be added later).
+    ///             Example: "en" means English, "en-US" represents US English.
+    ///             If the parameter is omitted, the paywall will be returned in the default locale.
     ///   - completion: A result containing the `AdaptyPaywall` object. This model contains the list of the products ids, paywall's identifier, custom payload, and several other properties.
     public static func getPaywall(_ id: String,
                                   locale: String? = nil,
