20c60189d39855d6868b29797dcc074c0f29b19b
Aleksei Valiano <vallianos@gmail.com>
2023-07-10 18:25:03 +0200

    AdaptyUI: add alternative coding CornerRadius as object

--- Sources/AdaptyUI/Format/Shape.CornerRadius+Decodable.swift
+++ Sources/AdaptyUI/Format/Shape.CornerRadius+Decodable.swift
@@ -9,18 +9,39 @@
 import Foundation
 
 extension AdaptyUI.Shape.CornerRadius: Decodable {
+    struct Object: Decodable {
+        let topLeft: Double?
+        let topRight: Double?
+        let bottomRight: Double?
+        let bottomLeft: Double?
+
+        enum CodingKeys: String, CodingKey {
+            case topLeft = "tl"
+            case topRight = "tr"
+            case bottomRight = "br"
+            case bottomLeft = "bl"
+        }
+    }
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         if let value = try? container.decode(Double.self) {
             self = .same(value)
         } else if let values = try? container.decode([Double].self) {
-            if values.isEmpty {
-                self = .none
-            } else if values.count < 4 {
-                self = .same(values[0])
-            } else {
-                self = .different(topLeft: values[0], topRight: values[1], bottomRight: values[2], bottomLeft: values[3])
+            switch values.count {
+            case 0: self = .none
+            case 1: self = .same(values[0])
+            case 2: self = .different(topLeft: values[0], topRight: values[1], bottomRight: 0.0, bottomLeft: 0.0)
+            case 3: self = .different(topLeft: values[0], topRight: values[1], bottomRight: values[2], bottomLeft: 0.0)
+            default: self = .different(topLeft: values[0], topRight: values[1], bottomRight: values[2], bottomLeft: values[3])
             }
+        } else if let value = try? container.decode(Object.self) {
+            self = .different(
+                topLeft: value.topLeft ?? 0.0,
+                topRight: value.topRight ?? 0.0,
+                bottomRight: value.bottomRight ?? 0.0,
+                bottomLeft: value.bottomLeft ?? 0.0
+            )
         } else {
             self = .none
         }
