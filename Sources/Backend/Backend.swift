241f0e8e93867d64c89e363195105c82e31e2dea
Aleksei Valiano <vallianos@gmail.com>
2023-11-17 13:30:05 +0100

    change useProtocolCachePolicy

--- Sources/Backend/Backend.swift
+++ Sources/Backend/Backend.swift
@@ -20,7 +20,7 @@ struct Backend: HTTPCodableConfiguration {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = Request.globalHeaders(secretKey: secretKey)
         configuration.timeoutIntervalForRequest = 30
-        configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        configuration.requestCachePolicy = .useProtocolCachePolicy
         if let (host, port) = withProxy {
             configuration.connectionProxyDictionary = [
                 String(kCFNetworkProxiesHTTPEnable): NSNumber(value: 1),
