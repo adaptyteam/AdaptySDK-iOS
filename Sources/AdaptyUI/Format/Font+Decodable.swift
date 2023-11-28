8b43f3a5c8d15bd501457772efdee0c49c2cefab
Aleksei Valiano <vallianos@gmail.com>
2023-11-28 19:44:16 +0100

    add custom fonts

--- Sources/AdaptyUI/Format/Font+Decodable.swift
+++ Sources/AdaptyUI/Format/Font+Decodable.swift
@@ -10,8 +10,11 @@ import Foundation
 
 extension AdaptyUI.Font: Decodable {
     enum CodingKeys: String, CodingKey {
-        case name = "value"
-        case style
+        case alias = "value"
+        case familyName = "family_name"
+        case weight
+        case italic
+//        case style // Deprecated
         case defaultSize = "size"
         case defaultColor = "color"
         case defaultHorizontalAlign = "horizontal_align"
@@ -19,8 +22,18 @@ extension AdaptyUI.Font: Decodable {
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-        style = try container.decode(String.self, forKey: .style)
+        if let v = (try? container.decode([String].self, forKey: .alias))?.first {
+            alias = v
+        } else {
+            alias = try container.decode(String.self, forKey: .alias)
+        }
+        if let v = (try? container.decode([String].self, forKey: .familyName))?.first {
+            familyName = v
+        } else {
+            familyName = try container.decode(String.self, forKey: .familyName)
+        }
+        weight = try container.decodeIfPresent(Int.self, forKey: .weight)
+        italic = (try container.decodeIfPresent(Bool.self, forKey: .italic)) ?? false
         defaultSize = try container.decodeIfPresent(Double.self, forKey: .defaultSize)
         defaultColor = try container.decodeIfPresent(AdaptyUI.Color.self, forKey: .defaultColor)
         defaultHorizontalAlign = try container.decodeIfPresent(AdaptyUI.HorizontalAlign.self, forKey: .defaultHorizontalAlign)
