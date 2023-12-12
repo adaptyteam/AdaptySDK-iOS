3394ccbeb210396b230b004aa8d6b36016f605f4
Alexey Goncharov <x401om@gmail.com>
2023-12-12 17:44:48 +0100

    update version to 2.9.1

--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public static let SDKVersion = "2.9.0"
+    public static let SDKVersion = "2.9.1"
 
     public static func setCrossPlatformSDK(version: String, name: String) {
         Environment.CrossPlatformSDK.name = name
