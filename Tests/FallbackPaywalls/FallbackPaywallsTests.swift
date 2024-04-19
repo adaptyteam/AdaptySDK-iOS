41ddf5b355008057e295f4b7942847b578bc0b10
Aleksei Valiano <vallianos@gmail.com>
2024-04-19 17:47:43 +0200

    upd unit tests

--- Tests/FallbackPaywalls/FallbackPaywallsTests.swift
+++ Tests/FallbackPaywalls/FallbackPaywallsTests.swift
@@ -10,8 +10,8 @@ import XCTest
 
 final class FallbackPaywallsTests: XCTestCase {
     enum Json: String {
-        case fallbacks1 = "fallbacks1.json"
-        case fallbacks2 = "fallbacks2.json"
+        case fallback_serialized = "fallbacks1.json"
+        case fallback = "fallbacks2.json"
 
         var url: URL {
             let thisSourceFile = URL(fileURLWithPath: #file)
@@ -26,9 +26,40 @@ final class FallbackPaywallsTests: XCTestCase {
             print("# Time elapsed for loadData: \(String(format: "%.6f", timeElapsed)) s.")
             return data
         }
+
+        func test(_ case: XCTestCase, paywallsId: String) throws {
+            try test(`case`, paywallsIds: [paywallsId])
+        }
+
+        func test(_ case: XCTestCase, paywallsIds: [String]) throws {
+            let expectation = `case`.expectation(description: "wait setFallbackPaywalls")
+
+            let data = try getData()
+
+            let startTime = CFAbsoluteTimeGetCurrent()
+            Adapty.setFallbackPaywalls(data) { _ in
+                let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+                print("## Time elapsed for setFallbackPaywalls: \(String(format: "%.6f", timeElapsed)) s.")
+
+                expectation.fulfill()
+            }
+
+            `case`.wait(for: [expectation], timeout: 10.0)
+
+            let fallbackPaywalls = Adapty.Configuration.fallbackPaywalls!
+
+            paywallsIds.forEach {
+                let startTime = CFAbsoluteTimeGetCurrent()
+                let paywall = fallbackPaywalls.getPaywall(byPlacmentId: $0, profileId: "unknown")
+                let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+                print("### Time elapsed for paywall[\($0)] \(paywall?.value.viewConfiguration != nil ? "paywall_builder" : ""): \(String(format: "%.6f", timeElapsed)) s.")
+
+                XCTAssertNotNil(paywall)
+            }
+        }
     }
 
-    static let paywallsIds = [
+    let paywallsIds = [
         "access.or.subscribe",
         "accesss",
         "all-onboarding",
@@ -51,57 +82,29 @@ final class FallbackPaywallsTests: XCTestCase {
         "yealy-onboarding",
     ]
 
-    func testFallback1() throws {
-        let expectation = expectation(description: "wait setFallbackPaywalls")
+    func testFallback_serialized_inMemorry() throws {
+        try Json.fallback_serialized.test(self, paywallsIds: paywallsIds)
+    }
 
-        let data = try Json.fallbacks1.getData()
+    func testFallback_inMemorry() throws {
+        try Json.fallback.test(self, paywallsIds: paywallsIds)
+    }
 
-        let startTime = CFAbsoluteTimeGetCurrent()
-        Adapty.setFallbackPaywalls(data) { _ in
-            let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-            print("## Time elapsed for setFallbackPaywalls: \(String(format: "%.6f", timeElapsed)) s.")
-
-            expectation.fulfill()
-        }
-
-        wait(for: [expectation], timeout: 10.0)
-
-        let fallbackPaywalls = Adapty.Configuration.fallbackPaywalls!
-
-        Self.paywallsIds.forEach {
+    func testFallback_serialized_reloadFile() throws {
+        try paywallsIds.forEach { id in
             let startTime = CFAbsoluteTimeGetCurrent()
-            let paywall = fallbackPaywalls.getPaywall(byPlacmentId: $0, profileId: "unknown")
+            try Json.fallback_serialized.test(self, paywallsId: id)
             let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-            print("### Time elapsed for paywall[\($0)] \(paywall?.value.viewConfiguration != nil ? "paywall_builder" : ""): \(String(format: "%.6f", timeElapsed)) s.")
-
-            XCTAssertNotNil(paywall)
+            print("#### Total time elapsed for paywall[\(id)]: \(String(format: "%.6f", timeElapsed)) s.")
         }
     }
 
-    func testFallback2() throws {
-        let expectation = expectation(description: "wait setFallbackPaywalls")
-
-        let data = try Json.fallbacks2.getData()
-
-        let startTime = CFAbsoluteTimeGetCurrent()
-        Adapty.setFallbackPaywalls(data) { _ in
-            let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-            print("Time elapsed for setFallbackPaywalls: \(String(format: "%.6f", timeElapsed)) s.")
-
-            expectation.fulfill()
-        }
-        wait(for: [expectation], timeout: 10.0)
-
-        let fallbackPaywalls = Adapty.Configuration.fallbackPaywalls!
-
-        Self.paywallsIds.forEach {
+    func testFallback_reloadFile() throws {
+        try paywallsIds.forEach { id in
             let startTime = CFAbsoluteTimeGetCurrent()
-
-            let paywall = fallbackPaywalls.getPaywall(byPlacmentId: $0, profileId: "unknown")
-
+            try Json.fallback.test(self, paywallsId: id)
             let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-            print("Time elapsed for paywall[\($0)] \(paywall?.value.viewConfiguration != nil ? "paywall_builder" : ""): \(String(format: "%.6f", timeElapsed)) s.")
-            XCTAssertNotNil(paywall)
+            print("#### Total time elapsed for paywall[\(id)]: \(String(format: "%.6f", timeElapsed)) s.")
         }
     }
 }
