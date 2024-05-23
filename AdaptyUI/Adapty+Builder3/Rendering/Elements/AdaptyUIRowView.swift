63256517862de76738f9639192d8661b8436260d
Alexey Goncharov <x401om@gmail.com>
2024-05-23 18:37:12 +0100

    AdaptyUI: AdaptyUIRowView refactoring

--- AdaptyUI/Adapty+Builder3/Rendering/Elements/AdaptyUIRowView.swift
+++ AdaptyUI/Adapty+Builder3/Rendering/Elements/AdaptyUIRowView.swift
@@ -11,42 +11,6 @@ import Adapty
 import SwiftUI
 
 @available(iOS 15.0, *)
-struct GeometrySizePreferenceKey: PreferenceKey {
-    static var defaultValue: CGSize = .zero
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
-        value = nextValue()
-    }
-}
-
-@available(iOS 15.0, *)
-struct AdaptyUIGeometrySizeObserver: ViewModifier {
-    var onChange: (CGSize) -> Void
-
-    init(_ onChange: @escaping (CGSize) -> Void) {
-        self.onChange = onChange
-    }
-
-    func body(content: Content) -> some View {
-        content
-            .background {
-                GeometryReader { proxy in
-                    Color
-                        .clear
-                        .preference(key: GeometrySizePreferenceKey.self, value: proxy.size)
-                        .onPreferenceChange(GeometrySizePreferenceKey.self) { onChange($0) }
-                }
-            }
-    }
-}
-
-@available(iOS 15.0, *)
-extension View {
-    func onGeometrySizeChange(perform action: @escaping (CGSize) -> Void) -> some View {
-        modifier(AdaptyUIGeometrySizeObserver(action))
-    }
-}
-
-@available(iOS 15.0, *)
 struct AdaptyUIRowView: View {
     @Environment(\.adaptyScreenSize)
     private var screenSize: CGSize
@@ -86,24 +50,26 @@ struct AdaptyUIRowView: View {
             let (totalWeight, reservedLength) = calculateTotalWeight(for: row.items, in: proxy)
             let weightsAvailableLength = proxy.size.width - reservedLength
 
-            LazyVGrid(columns: row.items.map { item in
-                switch item {
-                case let .fixed(length, content):
-                    return .init(.fixed(length.points(screenSize: screenSize.width)),
-                                 spacing: row.spacing,
-                                 alignment: .center) // TODO: apply row alignment
-                case let .flexible(weight, content):
-                    return .init(.fixed((Double(weight) / Double(totalWeight)) * weightsAvailableLength),
-                                 spacing: row.spacing,
-                                 alignment: .topTrailing) // TODO: apply row alignment
+            LazyVGrid(
+                columns: row.items.map { item in
+                    switch item {
+                    case let .fixed(length, content):
+                        return .init(.fixed(length.points(screenSize: screenSize.width)),
+                                     spacing: row.spacing,
+                                     alignment: .center) // TODO: apply row alignment
+                    case let .flexible(weight, content):
+                        return .init(.fixed((Double(weight) / Double(totalWeight)) * weightsAvailableLength),
+                                     spacing: row.spacing,
+                                     alignment: .center) // TODO: apply row alignment
+                    }
+                },
+                spacing: 0.0,
+                content: {
+                    ForEach(0 ..< row.items.count, id: \.self) { idx in
+                        AdaptyUIElementView(row.items[idx].content)
+                    }
                 }
-            },
-            spacing: 0.0,
-            content: {
-                ForEach(0 ..< row.items.count, id: \.self) { idx in
-                    AdaptyUIElementView(row.items[idx].content)
-                }
-            })
+            )
             .onGeometrySizeChange { contentsSize = $0 }
         }
         .frame(height: contentsSize.height)
@@ -123,7 +89,7 @@ struct AdaptyUIRowView: View {
                     verticalAlignment: .bottom,
                     spacing: 6.0,
                     items: [
-//                        .flexible(weight: 1, content: .text(.testBodyShort, nil)),
+                        //                        .flexible(weight: 1, content: .text(.testBodyShort, nil)),
 //                        .fixed(length: .point(32), content: .text(.init([
 //                            .text("Hello world! Hello world! Hello world!", .testBody),
 //                        ]), nil)),
