ef07dea8fefd0acc606736e8ef3e5f778167690c
Alexey Goncharov <x401om@gmail.com>
2024-06-17 15:47:34 +0100

    AdaptyUI: Add TransitionIn modifier

--- AdaptyUI/Rendering/Elements/AdaptyUIElementView.swift
+++ AdaptyUI/Rendering/Elements/AdaptyUIElementView.swift
@@ -23,13 +23,42 @@ extension View {
 }
 
 @available(iOS 15.0, *)
+extension AdaptyUI.Element {
+    var properties: AdaptyUI.Element.Properties? {
+        switch self {
+        case .space:
+            return nil
+        case let .box(_, properties), let .stack(_, properties),
+             let .text(_, properties), let .image(_, properties),
+             let .button(_, properties), let .row(_, properties),
+             let .column(_, properties), let .section(_, properties),
+             let .toggle(_, properties), let .timer(_, properties),
+             let .pager(_, properties), let .unknown(_, properties):
+            return properties
+        }
+    }
+}
+
+@available(iOS 15.0, *)
 package struct AdaptyUIElementView: View {
-    var element: AdaptyUI.Element
-    var additionalPadding: EdgeInsets?
+    private var element: AdaptyUI.Element
+    private var additionalPadding: EdgeInsets?
 
     package init(_ element: AdaptyUI.Element, additionalPadding: EdgeInsets? = nil) {
         self.element = element
         self.additionalPadding = additionalPadding
+    }
+
+    package var body: some View {
+        let properties = element.properties
+
+        elementBody
+            .paddingIfNeeded(additionalPadding)
+            .applyingProperties(properties)
+            .transitionIn(
+                properties?.transitionIn,
+                visibility: properties?.visibility ?? true
+            )
     }
 
     @ViewBuilder
@@ -41,7 +70,8 @@ package struct AdaptyUIElementView: View {
         }
     }
 
-    package var body: some View {
+    @ViewBuilder
+    private var elementBody: some View {
         switch element {
         case let .space(count):
             if count > 0 {
@@ -53,52 +83,28 @@ package struct AdaptyUIElementView: View {
             elementOrEmpty(box.content)
                 .fixedFrame(box: box)
                 .rangedFrame(box: box)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .stack(stack, properties):
             AdaptyUIStackView(stack)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .text(text, properties):
             AdaptyUITextView(text)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .image(image, properties):
             AdaptyUIImageView(image)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .button(button, properties):
             AdaptyUIButtonView(button)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .row(row, properties):
             AdaptyUIRowView(row)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .column(column, properties):
             AdaptyUIColumnView(column)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .section(section, properties):
             AdaptyUISectionView(section)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .toggle(toggle, properties):
             AdaptyUIToggleView(toggle)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .timer(timer, properties):
             AdaptyUITimerView(timer)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .pager(pager, properties):
             AdaptyUIPagerView(pager)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         case let .unknown(value, properties):
             AdaptyUIUnknownElementView(value: value)
-                .paddingIfNeeded(additionalPadding)
-                .applyingProperties(properties)
         }
     }
 }
