80ed0f5bc05874cf0ac8538229eec3cf63d287ae
Aleksei Valiano <vallianos@gmail.com>
2024-06-27 16:10:27 +0200

    fix

--- Sources/Backend/Requests/FetchAllProductVendorIdsRequest.swift
+++ Sources/Backend/Requests/FetchAllProductVendorIdsRequest.swift
@@ -10,13 +10,11 @@ import Foundation
 struct FetchAllProductVendorIdsRequest: HTTPRequestWithDecodableResponse {
     typealias ResponseBody = Backend.Response.ValueOfData<[String]>
     let endpoint: HTTPEndpoint
-    let queryItems: QueryItems
 
-    init(apiKeyPrefix: String, disableServerCache: Bool) {
+    init(apiKeyPrefix: String) {
         endpoint = HTTPEndpoint(
             method: .get,
             path: "/sdk/in-apps/\(apiKeyPrefix)/products-ids/app_store/"
         )
-        queryItems = QueryItems().setDisableServerCache(disableServerCache)
     }
 }
