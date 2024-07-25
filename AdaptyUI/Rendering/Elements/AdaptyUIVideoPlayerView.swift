59e2d6fecbc10912c01a14223107bdb84e556215
Alexey Goncharov <x401om@gmail.com>
2024-07-25 20:04:06 +0100

    AdaptyUI: add experimental video component

---
+++ AdaptyUI/Rendering/Elements/AdaptyUIVideoPlayerView.swift
@@ -0,0 +1,202 @@
+//
+//  SwiftUIView.swift
+//
+//
+//  Created by Aleksey Goncharov on 25.07.2024.
+//
+
+#if canImport(UIKit)
+
+import Adapty
+import AVKit
+import Combine
+import SwiftUI
+
+extension AdaptyUI.AspectRatio {
+    var videoGravity: AVLayerVideoGravity {
+        switch self {
+        case .fit: .resizeAspect
+        case .fill: .resizeAspectFill
+        case .stretch: .resize
+        }
+    }
+}
+
+@available(iOS 15.0, *)
+class AdaptyUIVideoPlayerManager: NSObject, ObservableObject {
+    enum PlayerState {
+        case invalid
+
+        case loading
+        case ready
+        case failed(String)
+
+        var title: String {
+            switch self {
+            case .invalid: "Invalid"
+            case .loading: "Loading"
+            case .ready: "Ready"
+            case let .failed(error): "Failed: \(error)"
+            }
+        }
+    }
+
+    @Published var playerState: PlayerState = .invalid
+    @Published var player: AVQueuePlayer?
+
+    private var playerLooper: AVPlayerLooper?
+
+    init(video: AdaptyUI.VideoPlayer) {
+        let playerToObserve: AVQueuePlayer?
+        let playerItemToObserve: AVPlayerItem?
+
+        switch video.asset {
+        case let .url(url, image):
+            let playerItem = AVPlayerItem(url: url)
+            let queuePlayer = AVQueuePlayer(items: [playerItem])
+
+            if video.loop {
+                playerLooper = AVPlayerLooper(player: queuePlayer, templateItem: playerItem)
+            }
+
+            player = queuePlayer
+
+            playerToObserve = queuePlayer
+            playerItemToObserve = playerItem
+
+            queuePlayer.play()
+        case let .resources(string, image):
+            playerToObserve = nil
+            playerItemToObserve = nil
+        }
+
+        super.init()
+
+        if let playerItemToObserve {
+            playerItemToObserve.addObserver(self, forKeyPath: "status", options: [.old, .new], context: nil)
+        }
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "status", let playerItem = object as? AVPlayerItem {
+            switch playerItem.status {
+            case .unknown:
+                playerState = .loading
+            case .readyToPlay:
+                playerState = .ready
+            case .failed:
+                if let error = playerItem.error {
+                    playerState = .failed(error.localizedDescription)
+                } else {
+                    playerState = .failed("unknown error")
+                }
+            }
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+
+    deinit {
+        player?.currentItem?.removeObserver(self, forKeyPath: "status")
+    }
+}
+
+@available(iOS 15.0, *)
+struct AdaptyUIVideoPlayerView: UIViewControllerRepresentable {
+    var player: AVPlayer
+    var videoGravity: AVLayerVideoGravity
+
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        playerViewController.videoGravity = videoGravity
+        playerViewController.view.backgroundColor = .clear
+
+        return playerViewController
+    }
+
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {}
+
+    static func dismantleUIViewController(_ uiViewController: AVPlayerViewController, coordinator: ()) {
+        uiViewController.player?.pause()
+        uiViewController.player = nil
+    }
+}
+
+@available(iOS 15.0, *)
+struct AdaptyUIVideoView: View {
+    @StateObject private var videoPlayerManager: AdaptyUIVideoPlayerManager
+
+    var video: AdaptyUI.VideoPlayer
+
+    init(video: AdaptyUI.VideoPlayer) {
+        self.video = video
+        _videoPlayerManager = StateObject(wrappedValue: AdaptyUIVideoPlayerManager(video: video))
+    }
+
+    var body: some View {
+        VStack {
+            if let player = videoPlayerManager.player {
+                AdaptyUIVideoPlayerView(
+                    player: player,
+                    videoGravity: video.aspect.videoGravity
+                )
+            }
+
+            Text("State: " + videoPlayerManager.playerState.title)
+        }
+    }
+}
+
+#if DEBUG
+
+extension AdaptyUI.VideoPlayer {
+    private static let url1 = URL(string: "https://firebasestorage.googleapis.com/v0/b/api-8970033217728091060-294809.appspot.com/o/Paywall%20video%201.mp4?alt=media&token=5e7ac250-091e-4bb3-8a99-6ac4f0735b37")!
+
+    private static let url2 = URL(string: "https://firebasestorage.googleapis.com/v0/b/api-8970033217728091060-294809.appspot.com/o/Paywall%20video%202.mp4?alt=media&token=8735a549-d035-432f-b609-fe795bfb4efb")!
+
+    private static let url3 = URL(string: "https://firebasestorage.googleapis.com/v0/b/api-8970033217728091060-294809.appspot.com/o/Paywall%20video%203.mov?alt=media&token=ba0e2ec6-f81e-424f-84e6-e18617bedfbf")!
+
+    static let test1 = AdaptyUI.VideoPlayer.create(
+        asset: .url(url1, image: .raster(Data())),
+        aspect: .stretch,
+        loop: true
+    )
+
+    static let test2 = AdaptyUI.VideoPlayer.create(
+        asset: .url(url2, image: .raster(Data())),
+        aspect: .fit,
+        loop: false
+    )
+    static let test3 = AdaptyUI.VideoPlayer.create(
+        asset: .url(url3, image: .raster(Data())),
+        aspect: .fill,
+        loop: true
+    )
+}
+
+@available(iOS 15.0, *)
+#Preview {
+    VStack {
+        AdaptyUIVideoView(video: .test1)
+        AdaptyUIVideoView(video: .test2)
+        AdaptyUIVideoView(video: .test3)
+    }
+}
+
+@available(iOS 15.0, *)
+public struct AdaptyUIVideoTestView: View {
+    public init() {}
+
+    public var body: some View {
+        VStack {
+            AdaptyUIVideoView(video: .test1)
+            AdaptyUIVideoView(video: .test2)
+            AdaptyUIVideoView(video: .test3)
+        }
+    }
+}
+
+#endif
+
+#endif
