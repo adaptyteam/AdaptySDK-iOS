f3a7be25f9ed0b641c13392e58c3ff466eb23283
Alexey Goncharov <x401om@gmail.com>
2024-08-02 13:45:30 +0100

    update SDK version to 3.0.1

--- AdaptyCrossPlatformCommon.podspec
+++ AdaptyCrossPlatformCommon.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AdaptyCrossPlatformCommon'
-  s.version          = '3.0.0'
+  s.version          = '3.0.1'
   s.summary          = 'Common files for cross-platform SDKs Adapty'
 
   s.description      = <<-DESC
--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public static let SDKVersion = "3.0.0"
+    public static let SDKVersion = "3.0.1"
     static let fallbackFormatVersion = 6
 
     public static func setCrossPlatformSDK(version: String, name: String) {
