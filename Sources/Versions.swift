6b1b72fe9e3925c829fae833706c0818c85c0171
Alexey Goncharov <x401om@gmail.com>
2024-08-08 14:04:22 +0100

    update version to 3.0.3-SNAPSHOT

--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public static let SDKVersion = "3.0.2"
+    public static let SDKVersion = "3.0.3-SNAPSHOT"
     static let fallbackFormatVersion = 6
 
     public static func setCrossPlatformSDK(version: String, name: String) {
