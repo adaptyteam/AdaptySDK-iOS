9fc359d2d3569c1b3153a9a1f59fce7c0499a0bc
Alexey Goncharov <x401om@gmail.com>
2024-09-30 17:33:31 +0100

    AdaptyUI: add isMuted to AVQueuePlayer

--- AdaptyUI/Logic/AdaptyVideoViewModel.swift
+++ AdaptyUI/Logic/AdaptyVideoViewModel.swift
@@ -90,6 +90,7 @@ class AdaptyUIVideoPlayerManager: NSObject, ObservableObject {
         case let .url(url, image):
             let playerItem = AVPlayerItem(url: url)
             let queuePlayer = AVQueuePlayer(items: [playerItem])
+            queuePlayer.isMuted = true
 
             if video.loop {
                 playerLooper = AVPlayerLooper(player: queuePlayer, templateItem: playerItem)
