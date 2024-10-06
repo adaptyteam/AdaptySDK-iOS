ab838c174936dd4069134fdf663198887ec4d01e
Alexey Goncharov <x401om@gmail.com>
2024-10-06 17:40:02 +0100

    fix offer type check

--- Sources/StoreKit/Entities.SK2/SK2Product.SubscriptionOffer.OfferType.swift
+++ Sources/StoreKit/Entities.SK2/SK2Product.SubscriptionOffer.OfferType.swift
@@ -17,9 +17,9 @@ extension SK2Product.SubscriptionOffer.OfferType {
             return .promotional
         default:
             #if compiler(>=6.0)
-                if #available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *), type == .winBack {
-                    return .winBack
-                }
+            if #available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *), self == .winBack {
+                return .winBack
+            }
             #endif
             return .unknown
         }
