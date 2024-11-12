298f315c457c28c37091d7e1988a031486d8fcea
Aleksei Valiano <vallianos@gmail.com>
2024-11-12 16:37:03 +0100

    Done Request.GetProfile + AdaptyProfile

--- CrossPlatformCommon/Codable/AdaptyPluginPaywallProduct+Codable.swift
+++ CrossPlatformCommon/Codable/AdaptyPluginPaywallProduct+Codable.swift
@@ -32,17 +32,11 @@ extension Request {
 private enum CodingKeys: String, CodingKey {
     case vendorProductId = "vendor_product_id"
     case adaptyProductId = "adapty_product_id"
-
-    case offerTypeWithIdentifier = "subscription_offer"
-
-    case promotionalOfferId = "promotional_offer_id"
-
     case paywallVariationId = "paywall_variation_id"
     case paywallABTestName = "paywall_ab_test_name"
     case paywallName = "paywall_name"
-
-    case subscriptionDetails = "subscription_details"
-
+    case offerTypeWithIdentifier = "subscription_offer"
+    case subscription
     case localizedDescription = "localized_description"
     case localizedTitle = "localized_title"
     case price
@@ -52,12 +46,69 @@ private enum CodingKeys: String, CodingKey {
 
 extension Response {
     struct AdaptyPluginPaywallProduct: Encodable {
-        let adaptyPaywallProduct: AdaptyPaywallProduct
+        let wrapped: AdaptyPaywallProduct
 
-        init(_ adaptyPaywallProduct: AdaptyPaywallProduct) {
-            self.adaptyPaywallProduct = adaptyPaywallProduct
+        init(_ wrapped: AdaptyPaywallProduct) {
+            self.wrapped = wrapped
         }
 
-        func encode(to encoder: any Encoder) throws {}
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(wrapped.vendorProductId, forKey: .vendorProductId)
+            try container.encode(wrapped.adaptyProductId, forKey: .adaptyProductId)
+            try container.encode(wrapped.variationId, forKey: .paywallVariationId)
+            try container.encode(wrapped.paywallABTestName, forKey: .paywallABTestName)
+            try container.encode(wrapped.paywallName, forKey: .paywallName)
+            try container.encode(wrapped.localizedDescription, forKey: .localizedDescription)
+            try container.encode(wrapped.localizedTitle, forKey: .localizedTitle)
+            try container.encode(Price(from: wrapped), forKey: .price)
+            try container.encodeIfPresent(wrapped.regionCode, forKey: .regionCode)
+            try container.encode(wrapped.isFamilyShareable, forKey: .isFamilyShareable)
+            try container.encodeIfPresent(Subscription(product: wrapped), forKey: .subscription)
+        }
+    }
+}
+
+private struct Subscription: Sendable, Encodable {
+    let groupIdentifier: String
+    let period: AdaptySubscriptionPeriod
+    let localizedPeriod: String?
+    let offer: AdaptySubscriptionOffer?
+
+    init?(product: AdaptyPaywallProduct) {
+        guard let groupIdentifier = product.subscriptionGroupIdentifier,
+              let period = product.subscriptionPeriod
+        else { return nil }
+
+        self.groupIdentifier = groupIdentifier
+        self.period = period
+        localizedPeriod = product.localizedSubscriptionPeriod
+        offer = product.subscriptionOffer
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case groupIdentifier = "group_identifier"
+        case period = "period"
+        case localizedPeriod = "localized_period"
+        case offer
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(groupIdentifier, forKey: .groupIdentifier)
+        try container.encode(period, forKey: .period)
+        try container.encodeIfPresent(localizedPeriod, forKey: .localizedPeriod)
+        try container.encode(offer, forKey: .offer)
+    }
+}
+
+extension Price {
+    init(from product: some AdaptyProduct) {
+        self.init(
+            amount: product.price,
+            currencyCode: product.currencyCode,
+            currencySymbol: product.currencySymbol,
+            localizedString: product.localizedPrice
+        )
     }
 }
