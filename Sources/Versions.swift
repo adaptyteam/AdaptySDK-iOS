1ac29a8d1fa6867fe90a70583ce9e504dd8ba79d
Alexey Goncharov <x401om@gmail.com>
2024-12-18 11:48:30 +0000

    update version to 3.2.4

--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public nonisolated static let SDKVersion = "3.2.3"
+    public nonisolated static let SDKVersion = "3.2.4"
     static nonisolated let fallbackFormatVersion = 6
 }
 
