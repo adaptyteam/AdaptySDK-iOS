c61344b729688fbcdc8f00469378138fd31f5cde
Alexey Goncharov <x401om@gmail.com>
2025-01-29 16:02:28 +0000

    update version to 3.4.0-SNAPSHOT

--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public nonisolated static let SDKVersion = "3.3.4"
+    public nonisolated static let SDKVersion = "3.4.0-SNAPSHOT"
     nonisolated static let fallbackFormatVersion = 6
 }
 
