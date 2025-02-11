b118b3c33692958ef5d23debb16dc525d192dff8
Alexey Goncharov <x401om@gmail.com>
2025-02-11 14:27:10 +0000

    AdaptyUITesting: expose hidden AdaptyProfile fields for testing purposes

--- AdaptyUITesting/AdaptyUI+Testing.swift
+++ AdaptyUITesting/AdaptyUI+Testing.swift
@@ -28,7 +28,7 @@ public extension AdaptyViewConfigurationTestWrapper {
             templateId: templateId,
             locale: locale,
             isRightToLeft: isRightToLeft,
-            images: images,
+            images: [],
             colors: [
                 "$green_figma": .solidColor(.create(data: 0x3EBD78FF)),
                 "$green_figma_cc": .solidColor(.create(data: 0x3EBD78CC)),
@@ -127,9 +127,10 @@ public struct AdaptyUITestRendererView: View {
     let paywallConfiguration: AdaptyUI.PaywallConfiguration
 
     public init(
-        viewConfigurationWrapper: AdaptyViewConfigurationTestWrapper,
-        imageResolver: AdaptyImageAssetResolver?,
-        videoResolver: AdaptyVideoAssetResolver?
+        viewConfigurationWrapper: AdaptyViewConfigurationTestWrapper
+//        ,
+//        imageResolver: AdaptyImageAssetResolver?,
+//        videoResolver: AdaptyVideoAssetResolver?
     ) {
         viewConfiguration = viewConfigurationWrapper.value
 
@@ -141,8 +142,8 @@ public struct AdaptyUITestRendererView: View {
             observerModeResolver: nil,
             tagResolver: ["TEST_TAG": "Adapty"],
             timerResolver: nil,
-            imageResolver: imageResolver,
-            videoResolver: videoResolver
+            imageResolver: nil, // imageResolver,
+            videoResolver: nil // videoResolver
         )
     }
 
