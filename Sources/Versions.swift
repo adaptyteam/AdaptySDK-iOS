c162722e777733c377971a46dc2835136ff5ab54
Aleksei Valiano <vallianos@gmail.com>
2025-02-25 13:56:50 +0100

    change version to 3.4.0

--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public nonisolated static let SDKVersion = "3.3.6"
+    public nonisolated static let SDKVersion = "3.4.0"
     nonisolated static let fallbackFormatVersion = 7
 }
 
