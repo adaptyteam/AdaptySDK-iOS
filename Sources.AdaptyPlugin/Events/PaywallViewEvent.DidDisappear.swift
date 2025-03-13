0df258c5521f919ed5ebe2bcdc140968ff65ae9b
Alexey Goncharov <x401om@gmail.com>
2025-03-13 14:13:18 +0000

    AdaptyPlugin: add DidAppear and DidDisappear view events

---
+++ Sources.AdaptyPlugin/Events/PaywallViewEvent.DidDisappear.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  Adapty
+//
+//  Created by Alexey Goncharov on 3/13/25.
+//
+
+import Adapty
+import AdaptyUI
+import Foundation
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
+extension PaywallViewEvent {
+    struct DidDisappear: AdaptyPluginEvent {
+        let id = "paywall_view_did_disappear"
+        let view: AdaptyUI.View
+
+        enum CodingKeys: String, CodingKey {
+            case id
+            case view
+        }
+    }
+}
