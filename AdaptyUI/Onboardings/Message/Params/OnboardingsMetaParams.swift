e135d9080e9ae680bb7a9e08f0ed83a81b2c3777
Alexey Goncharov <x401om@gmail.com>
2025-04-17 18:29:44 +0100

    AdaptyUI: parse total_screens parameter in OnboardingsMetaParams

--- AdaptyUI/Onboardings/Message/Params/OnboardingsMetaParams.swift
+++ AdaptyUI/Onboardings/Message/Params/OnboardingsMetaParams.swift
@@ -12,17 +12,19 @@ public struct OnboardingsMetaParams: Sendable, Hashable {
     public let onboardingId: String
     public let screenClientId: String
     public let screenIndex: Int
+    public let screensTotal: Int
 
     init(_ body: BodyDecoder.Value) throws {
         let body = try body.asDictionary()
         self.onboardingId = try body["onboarding_id"].asString()
         self.screenClientId = try body["screen_cid"].asString()
         self.screenIndex = try body["screen_index"].asInt()
+        self.screensTotal = try body["total_screens"].asInt()
     }
 }
 
 extension OnboardingsMetaParams: CustomDebugStringConvertible {
     public var debugDescription: String {
-        "{onboardingId: \(onboardingId), screenClientId: \(screenClientId), screenIndex: \(screenIndex)}"
+        "{onboardingId: \(onboardingId), screenClientId: \(screenClientId), screenIndex: \(screenIndex), screensTotal: \(screensTotal)}"
     }
 }
