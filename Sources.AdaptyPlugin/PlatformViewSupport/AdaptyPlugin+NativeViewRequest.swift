2c685af8ad554e25e2e042b1a72eac7b869c2b8b
Alexey Goncharov <x401om@gmail.com>
2025-05-30 02:45:59 +0200

    AdaptyPlugin: fix Request.AdaptyUICreateNativeOnboardingView

--- Sources.AdaptyPlugin/PlatformViewSupport/AdaptyPlugin+NativeViewRequest.swift
+++ Sources.AdaptyPlugin/PlatformViewSupport/AdaptyPlugin+NativeViewRequest.swift
@@ -10,29 +10,16 @@ import Foundation
 
 private let log = Log.plugin
 
-extension Request {
-    struct AdaptyUICreateNativeOnboardingView: Decodable {
-        static let method = "adapty_ui_create_native_onboarding_view"
-
-        let onboarding: AdaptyOnboarding
-
-        enum CodingKeys: String, CodingKey {
-            case onboarding
-        }
-    }
-}
-
 // TODO: refactor this
 public extension AdaptyPlugin {
     static func executeCreateNativeOnboardingView(withJson jsonString: AdaptyJsonString) async -> AdaptyOnboarding? {
         do {
-            let request = try AdaptyPlugin.decoder.decode(
-                Request.AdaptyUICreateNativeOnboardingView.self,
+            return try AdaptyPlugin.decoder.decode(
+                AdaptyOnboarding.self,
                 from: jsonString.asAdaptyJsonData
             )
-            return request.onboarding
         } catch {
-            let error = AdaptyPluginError.decodingFailed(message: "Request params of method:\(Request.AdaptyUICreateNativeOnboardingView.method) is invalid", error)
+            let error = AdaptyPluginError.decodingFailed(message: "Request params of method: create_native_onboarding_view is invalid", error)
             log.error(error.message)
             return nil
         }
