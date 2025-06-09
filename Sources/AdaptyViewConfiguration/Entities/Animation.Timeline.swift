fb34d6c94961b15ea94b5168793f6a1573570fed
Aleksei Valiano <vallianos@gmail.com>
2025-06-09 11:40:06 +0200

    Animations: use loop

--- Sources/AdaptyViewConfiguration/Entities/Animation.Timeline.swift
+++ Sources/AdaptyViewConfiguration/Entities/Animation.Timeline.swift
@@ -13,20 +13,22 @@ package extension AdaptyViewConfiguration.Animation {
             duration: 0.3,
             interpolator: .default,
             startDelay: 0.0,
-            repeatType: nil,
-            repeatDelay: 0.0,
+            loop: nil,
+            loopDelay: 0.0,
+            pingPongDelay: 0.0,
             repeatMaxCount: nil,
         )
         package let duration: TimeInterval
         package let interpolator: Interpolator
         package let startDelay: TimeInterval
-        package let repeatType: RepeatType?
-        package let repeatDelay: TimeInterval
+        package let loop: Loop?
+        package let loopDelay: TimeInterval
+        package let pingPongDelay: TimeInterval
         package let repeatMaxCount: Int?
 
-        package enum RepeatType: String {
-            case restart
-            case reverse
+        package enum Loop: String {
+            case normal
+            case pingPong = "ping_pong"
         }
     }
 }
@@ -37,16 +39,18 @@ package extension AdaptyViewConfiguration.Animation.Timeline {
         startDelay: TimeInterval = Self.default.startDelay,
         duration: TimeInterval = Self.default.duration,
         interpolator: AdaptyViewConfiguration.Animation.Interpolator = Self.default.interpolator,
-        repeatType: RepeatType? = Self.default.repeatType,
-        repeatDelay: TimeInterval = Self.default.repeatDelay,
+        loop: Loop? = Self.default.loop,
+        loopDelay: TimeInterval = Self.default.loopDelay,
+        pingPongDelay: TimeInterval = Self.default.pingPongDelay,
         repeatMaxCount: Int? = Self.default.repeatMaxCount
     ) -> Self {
         .init(
             duration: duration,
             interpolator: interpolator,
             startDelay: startDelay,
-            repeatType: repeatType,
-            repeatDelay: repeatDelay,
+            loop: loop,
+            loopDelay: loopDelay,
+            pingPongDelay: pingPongDelay,
             repeatMaxCount: repeatMaxCount
         )
     }
@@ -56,12 +60,12 @@ package extension AdaptyViewConfiguration.Animation.Timeline {
 extension AdaptyViewConfiguration.Animation.Timeline: Codable {
     enum CodingKeys: String, CodingKey {
         case startDelay = "start_delay"
-        case repeatType = "repeat"
-        case repeatDelay = "repeat_delay"
+        case loop
+        case loopDelay = "loop_delay"
+        case pingPongDelay = "ping_pong_delay"
         case repeatMaxCount = "repeat_max_count"
         case duration
         case interpolator
-
     }
 
     package init(from decoder: Decoder) throws {
@@ -69,15 +73,16 @@ extension AdaptyViewConfiguration.Animation.Timeline: Codable {
         let defaultValue = Self.default
         startDelay = try (container.decodeIfPresent(TimeInterval.self, forKey: .startDelay)).map { $0 / 1000.0 } ?? defaultValue.startDelay
 
-        repeatType = try container.decodeIfPresent(RepeatType.self, forKey: .repeatType) ?? defaultValue.repeatType
+        loop = try container.decodeIfPresent(Loop.self, forKey: .loop) ?? defaultValue.loop
 
-        repeatDelay = try (container.decodeIfPresent(TimeInterval.self, forKey: .repeatDelay)).map { $0 / 1000.0 } ?? defaultValue.repeatDelay
+        loopDelay = try (container.decodeIfPresent(TimeInterval.self, forKey: .loopDelay)).map { $0 / 1000.0 } ?? defaultValue.loopDelay
+
+        pingPongDelay = try (container.decodeIfPresent(TimeInterval.self, forKey: .pingPongDelay)).map { $0 / 1000.0 } ?? defaultValue.pingPongDelay
 
         repeatMaxCount = try container.decodeIfPresent(Int.self, forKey: .repeatMaxCount) ?? defaultValue.repeatMaxCount
 
         duration = try (container.decodeIfPresent(TimeInterval.self, forKey: .duration)).map { $0 / 1000.0 } ?? defaultValue.duration
         interpolator = try (container.decodeIfPresent(AdaptyViewConfiguration.Animation.Interpolator.self, forKey: .interpolator)) ?? .default
-
     }
 
     package func encode(to encoder: any Encoder) throws {
@@ -89,10 +94,14 @@ extension AdaptyViewConfiguration.Animation.Timeline: Codable {
             try container.encode(startDelay * 1000, forKey: .startDelay)
         }
 
-        try container.encodeIfPresent(repeatType?.rawValue, forKey: .repeatType)
+        try container.encodeIfPresent(loop?.rawValue, forKey: .loop)
 
-        if repeatDelay != defaultValue.repeatDelay {
-            try container.encode(repeatDelay * 1000, forKey: .repeatDelay)
+        if loopDelay != defaultValue.loopDelay {
+            try container.encode(loopDelay * 1000, forKey: .loopDelay)
+        }
+
+        if pingPongDelay != defaultValue.pingPongDelay {
+            try container.encode(pingPongDelay * 1000, forKey: .pingPongDelay)
         }
 
         try container.encodeIfPresent(repeatMaxCount, forKey: .repeatMaxCount)
@@ -100,11 +109,11 @@ extension AdaptyViewConfiguration.Animation.Timeline: Codable {
         if duration != defaultValue.duration {
             try container.encode(duration * 1000, forKey: .duration)
         }
-        
+
         if interpolator != defaultValue.interpolator {
             try container.encode(interpolator, forKey: .interpolator)
         }
     }
 }
 
-extension AdaptyViewConfiguration.Animation.Timeline.RepeatType: Codable {}
+extension AdaptyViewConfiguration.Animation.Timeline.Loop: Codable {}
