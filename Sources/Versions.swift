a835455e7a1be958d4a2d6c67b97e01f534f2c6f
Aleksei Valiano <vallianos@gmail.com>
2025-07-17 12:56:35 +0200

    Release 3.9.0: Update version numbers and dependencies

--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public nonisolated static let SDKVersion = "3.8.2"
+    public nonisolated static let SDKVersion = "3.9.0"
     nonisolated static let fallbackFormatVersion = 8
     nonisolated static let userAcquisitionVersion = 1
 }
