d065c051d03709da746f44c3ba42a5005adbed7c
Aleksei Valiano <vallianos@gmail.com>
2025-07-29 14:49:58 +0200

    Update version numbers and dependencies to 3.10.0-SNAPSHOT

--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public nonisolated static let SDKVersion = "3.10.0"
+    public nonisolated static let SDKVersion = "3.11.0-SNAPSHOT"
     nonisolated static let fallbackFormatVersion = 8
     nonisolated static let userAcquisitionVersion = 1
 }
