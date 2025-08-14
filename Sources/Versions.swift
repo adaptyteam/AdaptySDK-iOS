da74d744be24d1a3bbdfbe43861a7c665b51e357
Alexey Goncharov <x401om@gmail.com>
2025-08-14 14:50:27 +0100

    update version to 3.10.2

--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public nonisolated static let SDKVersion = "3.10.1"
+    public nonisolated static let SDKVersion = "3.10.2"
     nonisolated static let fallbackFormatVersion = 8
     nonisolated static let userAcquisitionVersion = 1
 }
