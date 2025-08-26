ba0934ab8dbf28d77a5687634fc681c9109c7f5f
Alexey Goncharov <x401om@gmail.com>
2025-08-26 17:42:54 +0100

    update version to 3.12.0

--- AdaptyUI.podspec
+++ AdaptyUI.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AdaptyUI'
-  s.version          = '3.11.0-SNAPSHOT'
+  s.version          = '3.12.0'
   s.summary          = 'Adapty SDK for iOS.'
 
   s.description      = <<-DESC
--- Sources/Versions.swift
+++ Sources/Versions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Adapty {
-    public nonisolated static let SDKVersion = "3.11.0-SNAPSHOT"
+    public nonisolated static let SDKVersion = "3.12.0"
     nonisolated static let fallbackFormatVersion = 9
     nonisolated static let userAcquisitionVersion = 1
 }
