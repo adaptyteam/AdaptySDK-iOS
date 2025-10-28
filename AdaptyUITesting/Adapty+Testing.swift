faa9563c0f3b22e634092eb68e6f2b20211b3da8
Alexey Goncharov <x401om@gmail.com>
2025-10-28 13:21:07 +0000

    AdaptyUITesting: add testing_requestLocaleId

--- AdaptyUITesting/Adapty+Testing.swift
+++ AdaptyUITesting/Adapty+Testing.swift
@@ -12,3 +12,7 @@ public extension AdaptyProfile {
     var testing_segmentId: String { segmentId }
     var testing_isTestUser: Bool { isTestUser }
 }
+
+public extension AdaptyOnboarding {
+    var testing_requestLocaleId: String { self.requestLocaleIdentifier }
+}
