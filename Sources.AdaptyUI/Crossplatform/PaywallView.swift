5e62d4bbd8f6e0a3def5359f52e1b9ea0b1cc7a0
Alexey Goncharov <x401om@gmail.com>
2026-01-09 09:41:30 +0000

    Crossplatform: AdaptyUI.PaywallView fix placementId value

--- Sources.AdaptyUI/Crossplatform/PaywallView.swift
+++ Sources.AdaptyUI/Crossplatform/PaywallView.swift
@@ -43,7 +43,7 @@ public extension AdaptyPaywallController {
         AdaptyUI.PaywallView(
             id: id,
             templateId: configuration.paywallViewModel.viewConfiguration.templateId,
-            placementId: configuration.paywallVariationId,
+            placementId: configuration.paywallPlacementId,
             variationId: configuration.paywallVariationId
         )
     }
