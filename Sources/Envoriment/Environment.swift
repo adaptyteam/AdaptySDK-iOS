7eb62d99d0c3f04c046b441f96da4f33c5824e25
Vallianos <vallianos@gmail.com>
2026-08-05 11:46:14 +0200

    Report app minimum OS version

--- Sources/Envoriment/Environment.swift
+++ Sources/Envoriment/Environment.swift
@@ -8,35 +8,47 @@
 import Foundation
 
 struct Environment: Sendable {
-    let application: (installationIdentifier: String, version: String?, build: String?)
-    let system: (name: String, version: String)
+    let application: (
+        installationIdentifier: String,
+        version: String?,
+        build: String?,
+        minimumOSVersion: String?
+    )
+    let system: (
+        name: String,
+        version: String
+    )
     let sessionIdentifier: String
+}
 
-    @AdaptyActor
-    private init() async {
-        application = (
-            installationIdentifier: Environment.Application.installationIdentifier,
-            version: Environment.Application.version,
-            build: Environment.Application.build
-        )
-        system = await (
-            name: Environment.System.name,
-            version: Environment.System.version
-        )
-
-        sessionIdentifier = Environment.Application.sessionIdentifier
-    }
-
-    @AdaptyActor
+@AdaptyActor
+extension Environment {
     static var _instance: Environment?
 
-    @AdaptyActor
     static var instance: Environment {
         get async {
-            if let instance = _instance { return instance }
+            if let instance = _instance {
+                return instance
+            }
             let instance = await Environment()
             _instance = instance
             return instance
         }
+    }
+
+    private init() async {
+        await self.init(
+            application: (
+                installationIdentifier: Environment.Application.installationIdentifier,
+                version: Environment.Application.version,
+                build: Environment.Application.build,
+                minimumOSVersion: Environment.Application.minimumOSVersion
+            ),
+            system: (
+                name: Environment.System.name,
+                version: Environment.System.version
+            ),
+            sessionIdentifier: Environment.Application.sessionIdentifier
+        )
     }
 }
